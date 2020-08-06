c6a5a1913eba9c413903f2adce38401214ff8165
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2020-08-06 12:22:02 +0100

    refact(index.js): code refactored

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const assert = require('assert')
 
-module.exports = function (coords) {
+/**
+ * @description Returns coordinates on the other side of the earth.
+ * @param {{latitude: number, longitude: number}} coords
+ * @returns {{latitude: number, longitude: number}}
+ */
+function antipodes (coords) {
   assert(coords)
   assert(coords.latitude)
   assert(coords.longitude)
-  const { longitude: lng, latitude: lat } = coords
-  const latitude = lat > 0 ? -lat : Math.abs(lat)
-  const longitude = lng > 0 ? lng - 180 : lng + 180
-  return { latitude, longitude }
+  const { longitude, latitude } = coords
+
+  return {
+    latitude: latitude > 0 ? -latitude : Math.abs(latitude),
+    longitude: longitude > 0 ? longitude - 180 : longitude + 180
+  }
 }
+
+module.exports = antipodes
