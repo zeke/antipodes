a02cd5536200ff9f27b8103a9268431471393864
Zeke Sikelianos <zeke@sikelianos.com>
2020-08-06 21:51:00 -0700

    add comments for context on implementations

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ module.exports = function (coords, opts = { implementation: 'leitzel' }) {
   const { longitude: lng, latitude: lat } = coords
 
   switch (opts.implementation) {
+    // https://github.com/joshleitzel/antipodes
     case 'leitzel':
       return {
         latitude: lat > 0 ? -lat : Math.abs(lat),
         longitude: lng > 0 ? lng - 180 : lng + 180
       }
+
+    // https://twitter.com/mourner/status/1291293509597757440
     case 'agafonkin':
       return {
         longitude: (lng + 360) % 360 - 180,
